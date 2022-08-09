df3d05ff4a277ff8058de297954b4f1368b774d4
Renzo D`Angelo <renzodang@gmail.com>
2022-08-09 18:29:08 -0300

    subida app js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-g
+const direccion ="https://japceibal.github.io/emercado-api/cats_products/101.json" ;
+
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    fetch(direccion)
+    .then(response => response.json())
+    .then(datos=> {
+        document.getElementById("Cat").innerHTML= datos.catName + "<br>";
+        document.getElementById("img1").innerHTML.src = datos.products[0].image;
+        document.getElementById("name1").innerHTML = datos.products[0].name;
+        document.getElementById("prize1").innerHTML = datos.products[0].cost;
+        document.getElementById("currency1").innerHTML= datos.products[0].currency;
+        document.getElementById("description1").innerHTML = datos.products[0].description
+        document.getElementById("sold1").innerHTML = datos.products[0].soldCount;
+      
+        });
+  
+});
